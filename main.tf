fe05b00050102416d6eaa8c5d52a2f14c275065a
Abdul Rawoof Mohammed <32982250+Rawoof-Abdul@users.noreply.github.com>
2024-07-22 09:22:55 -0500

    Initial commit

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
   required_version = ">= 0.12"
 }
 
@@ -7,11 +13,10 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-name"
+  bucket = "my-7950-bucket-name"
   acl    = "private"
 }
 
 output "bucket_name" {
   value = aws_s3_bucket.my_bucket.bucket
 }
-
