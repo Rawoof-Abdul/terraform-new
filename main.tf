2bf2cd4a6672256ec0c3b40c6d49c276761074f4
Abdul Rawoof Mohammed <32982250+Rawoof-Abdul@users.noreply.github.com>
2024-07-22 01:24:50 -0500

    first file

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 0.12"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-unique-bucket-name"
+  acl    = "private"
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.my_bucket.bucket
+}
+
